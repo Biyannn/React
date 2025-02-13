6522794c8059a2d6ce0468676e28e1f30c8a8734
Biyannn <ayyubian07@gmail.com>
2025-02-13 22:49:00 +0700

    feat:fitur baru

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import Container from "./Container"
 
 const App = () => {
@@ -19,9 +19,24 @@ const App = () => {
     }
   ]
 
-  const [, setCount] = useState(1)
+  const [count, setCount] = useState(1)
+  const [text, setText] = useState('')
+  const formDOM = useRef(null)
+
+  console.log('start')
+
+  useEffect(() => {
+    console.log('useEffect')
+  }, [])
+
   const handleClick = () => {
     setCount(count + 1)
+  } 
+
+  const handleSubmit = () => {
+    e.preventDefault()
+        console.log('submit')
+        console.log(e.target.username.value)
   }
   
   return (
@@ -29,8 +44,34 @@ const App = () => {
       {list.map((item, index) => (
             <Container key={index}title={item.title} desc={item.desc} />
       ))}
+
+      {console.log('render')}
+
       <h1>count: {count}</h1>
       <button onClick={handleClick}>klik</button>
+
+      <form ref={formDOM} style={{marginTop: 24}} onSubmit={handleSubmit}>
+
+        <input type="text"  name="username"/>
+        <button type="submit"> Submit</button>
+      </form>
+      <button onClick={() => formDOM.current.reset()}>reset form</button>
+
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <input type="text" placeholder="text" value={text} onChange={(e) => {
+        console.log('change', e.target.value )
+        setText(e.target.value)
+      }}/>
+
+      <button onClick={() => {
+        setText('')
+      }}>reset</button>
+
+      <p>{text}</p>
     </>
   )
 }
